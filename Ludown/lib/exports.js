83b4836af3ad970a5bd079d50b96a86c57ac04c2
Vishwac Sena Kannan <vkannan@microsoft.com>
2018-07-10 11:30:37 -0700

    Updating exports

--- Ludown/lib/exports.js
+++ Ludown/lib/exports.js
@@ -5,8 +5,14 @@
 export const translate = require('./translate-helpers');
 export const refresh = require('./toLU-helpers');
 export const parser = require('./parseFileContents');
-export const helpers = {
+export const helperEnums = {
     errorCodes: require('./enums/CLI-errors'),
     parseCommands: require('./enums/parsecommands'),
+};
+export const helperClasses = {
+    exception: require('./classes/error'),
+    LUIS: require('./classes/LUIS'),
+    QnA: require('./classes/qna'),
+    parser: require('./classes/parserObject')
 };
 
