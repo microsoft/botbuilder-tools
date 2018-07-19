b0425a568a2cd5c66407a1314b7a7150c27a4c71
Vishwac Sena Kannan <vkannan@microsoft.com>
2018-07-18 20:15:46 -0700

    Fix for #272 and pushing v1.0.32

--- QnAMaker/lib/api/dataModels/wordAlterationsDto.js
+++ QnAMaker/lib/api/dataModels/wordAlterationsDto.js
@@ -2,7 +2,7 @@
   * Copyright (c) Microsoft Corporation. All rights reserved.
   * Licensed under the MIT License.
   */
-
+const AlterationsDTO = require('./alterationsDto');
 
 class WordAlterationsDTO {
     
