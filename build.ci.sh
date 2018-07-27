54dc5f1ca9d03fa917fbb03ebecaf724e55f55b3
JuanAr <juan.arguello@southworks.com>
2018-07-27 19:47:00 +0200

    Enable Travis CI and Daily Builds (publish to MyGet)

---
+++ build.ci.sh
@@ -0,0 +1,58 @@
+function install() {
+    echo Install $1
+    pushd $1
+    npm install
+    if [ $2 ]; then npm run test; fi
+    popd
+}
+
+function create_npmrc() {
+    cat > .npmrc << EOF
+registry=https:\${NPM_REGISTRY}
+\${NPM_REGISTRY}:_authToken=\${NPM_TOKEN}
+EOF
+}
+
+function update_version() {
+    pname=$(npm show --json | jq -r '.name')
+    pversion=$(npm view $pname version)
+    npm version --allow-same-version $pversion
+    npm version prerelease
+}
+
+function publish() {
+    echo Publish $1
+    pushd $1
+    create_npmrc
+    update_version
+    npm install
+    npm publish
+    popd
+}
+
+if [[ "${TRAVIS_EVENT_TYPE}" = "cron" ]]; then
+    publish Chatdown
+    publish Dispatch
+    publish Ludown
+    publish LUIS
+    publish LUISGen
+    publish MSBot
+    publish QnAMaker
+else
+    (
+        set -e
+        install Chatdown with_test
+        install Dispatch
+        install Ludown with_test
+        install LUIS with_test
+        install LUISGen
+        install MSBot
+        install QnAMaker with_test
+    )
+fi
+errorcode=$?
+
+if [ $errorcode -ne 0 ]; then
+  echo exiting with errorcode $errorcode
+  exit $errorcode
+fi
