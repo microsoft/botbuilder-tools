73b79230abb8499ad8e1e0dc3ca93344b39d75e4
Enzo Cano <enzo.cano@southworks.com>
2018-08-14 19:23:22 -0300

    Improve code coverage

---
+++ packages/LUIS/test/luis.cli.test.suite.js
@@ -0,0 +1,47 @@
+const assert = require('assert');
+const fs = require('fs-extra');
+const path = require('path');
+const { exec, spawnSync } = require('child_process');
+const luis = require.resolve('../bin/luis');
+const ApplicationCreateObject = require.resolve('../examples/ApplicationCreateObject.json');
+
+describe('The LUIS cli tool', () => {
+
+    describe('should perform the correct action and display the contents', () => {
+
+        it('when using the "luis" arg', done =>{
+            exec(`node ${luis} luis list apps`, (error, stdout, stderr) => {
+                assert.equal(stdout, '');
+                assert(stderr.includes('luis list apps --skip <integer> --take <integer>'));
+                done();
+            });
+        });
+        
+        describe('with query command', () => {
+            it('and no question', done =>{
+                exec(`node ${luis} --authoringKey dummy-key --endpointBasePath dummy-path query`, (error, stdout, stderr) => {
+                    assert.equal(stdout, '');
+                    assert(stderr.includes('missing -q'));
+                    done();
+                });
+            });
+
+            it('and no appid', done =>{
+                exec(`node ${luis} --authoringKey dummy-key --endpointBasePath dummy-path query --q "message to send"`, (error, stdout, stderr) => {
+                    assert.equal(stdout, '');
+                    assert(stderr.includes('missing --appid'));
+                    done();
+                });
+            });
+
+            it('and all other args', done =>{
+                exec(`node ${luis} --authoringKey dummy-key --endpointBasePath dummy-path query --q "message to send" --appId dummy-app`, (error, stdout, stderr) => {
+                    assert.equal(stdout, '');
+                    assert(stderr.includes('401'), stderr);
+                    done();
+                });
+            });
+        });
+        
+    });
+});
