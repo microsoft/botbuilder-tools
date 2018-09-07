d05bca9e8a8ab920320f9240fabab86f55ba798c
Tom Laird-McConnell <tomlm@microsoft.com>
2018-09-07 13:20:21 -0700

    add package unit tests to verify that package is defined to include needed files

---
+++ packages/MSBot/test/msbot.package.test.suite.js
@@ -0,0 +1,46 @@
+const assert = require('assert');
+const fs = require('fs-extra');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const tgz = require('tgz');
+const enumFiles = require('enum-files');
+
+describe('Package test', async () => {
+    let packageName = '';
+    it('package should have all expected files', async () => {
+
+        let includedFolders = [
+            'bin',
+            'docs',
+            'typings'
+        ];
+
+        let expectedFiles = [];
+        for (var includedFolder of includedFolders) {
+            let files = await enumFiles.filesRecursively(`../${includedFolder}`);
+            for (let file of files) {
+                file = file.replace('..', 'package').replace(/\\/g, "/");
+                expectedFiles.push(file);
+            }
+        }
+
+        // make package
+        let result = await exec('npm pack ..');
+        packageName = result.stdout.trim('\n');
+        
+        await new Promise((resolve, reject) => {
+            tgz(packageName, (error, files) => {
+                for (let expectedFile of expectedFiles) {
+                    assert.ok(files.hasOwnProperty(expectedFile), `missing ${expectedFile}`);
+                }
+                resolve();
+            });
+        });
+    });
+
+    after(function () {
+        // runs after all tests in this block
+        fs.unlinkSync(packageName);
+    });
+});
+
