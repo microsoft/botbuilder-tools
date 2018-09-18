191e63d6757486071c29d5112d7cde57bda87fb5
David Favretto <david.f@hotmail.com.ar>
2018-09-18 15:10:16 -0300

    Add message with tool name as prefix for indentifyng

--- packages/Dispatch/bin/dispatch.js
+++ packages/Dispatch/bin/dispatch.js
@@ -5,16 +5,20 @@
  */
 const pkg = require('../package.json');
 let args = '';
+let prefix = false;
 
 process.argv.forEach((val, index) => {
     if (index > 1) {
         args = args + ' ' + val;
+        prefix = prefix || val == '--prefix';
     }
 });
 args = args.trim();
 if (args == '-v' || args == '--version') {
     return process.stdout.write(pkg.version);
 }
-
+if(prefix) {
+    process.stdout.write(`\n[${pkg.name}] Calling netcore DLL\n`);
+}
 process.argv= [process.argv[0], process.argv[1], __dirname + '/netcoreapp2.1/Dispatch.dll', ...process.argv.slice(2)]
 require("dotnet-2.1")
