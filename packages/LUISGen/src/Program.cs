d336a7db93f2256379c5635ff7e2618274aef9ab
Chris McConnell <chrimc@microsoft.com>
2018-09-24 15:34:09 -0700

    Add -v --version to commands. Reogranize to better support new structure. Update readmes.

--- packages/LUISGen/src/Program.cs
+++ packages/LUISGen/src/Program.cs
@@ -10,13 +10,14 @@ namespace LUISGen
     {
         static void Usage()
         {
-            Console.Error.WriteLine("LUISGen <LUIS.json> [-cs [CLASS]] [-ts [INTERFACE]] [-o PATH]");
+            Console.Error.WriteLine("LUISGen <LUIS.json> [-cs [CLASS]] [-ts [INTERFACE]] [-o PATH] [-v] [--version]");
             Console.Error.WriteLine("From a LUIS export file generate a strongly typed class for consuming intents and entities.");
             Console.Error.WriteLine("If the input is -, will get the export file from stdin.");
             Console.Error.WriteLine("At least one of -cs or -ts must be supplied.");
             Console.Error.WriteLine("-cs [CLASS] : Generate C# class file including namespace.  Default is Luis.<appName> if no class name is specified.");
             Console.Error.WriteLine("-ts [INTERFACE] : Generate Typescript interface descriptions.  Default is <appName> if no class name is specified.");
             Console.Error.WriteLine("-o PATH : Where to put generated files, defaults to directory where export file is.");
+            Console.Error.WriteLine("-v --version : Report the LUISGen version.");
             System.Environment.Exit(-1);
         }
 
@@ -67,52 +68,57 @@ namespace LUISGen
             string className = null;
             for (var i = 0; i < args.Length; ++i)
             {
-                var arg = NextArg(ref i, args, allowCmd:true);
+                var arg = NextArg(ref i, args, allowCmd: true);
                 if (arg != null)
                 {
-                    if (path == null)
+                    if (arg.StartsWith('-'))
+                    {
+                        arg = arg.ToLower();
+                        switch (arg)
+                        {
+                            case "-cs":
+                            case "-ts":
+                                {
+                                    ++i;
+                                    var name = NextArg(ref i, args, optional: true);
+                                    if (name != null)
+                                    {
+                                        var lastDot = arg == "-cs" ? name.LastIndexOf('.') : -1;
+                                        if (lastDot == -1)
+                                        {
+                                            className = name;
+                                        }
+                                        else
+                                        {
+                                            space = name.Substring(0, lastDot);
+                                            className = name.Substring(lastDot + 1);
+                                        }
+                                    }
+                                    outType = arg.Substring(1);
+                                }
+                                break;
+                            case "-o":
+                                {
+                                    ++i;
+                                    outPath = NextArg(ref i, args);
+                                }
+                                break;
+                            case "-v":
+                            case "--version":
+                                Console.WriteLine($"LUISGen version {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version}");
+                                break;
+                            default:
+                                Usage();
+                                break;
+                        }
+                    }
+                    else if (path == null)
                     {
                         path = arg;
                     }
                     else
                     {
-                        arg = arg.ToLower();
-                        if (arg.StartsWith('-'))
-                        {
-                            switch (arg)
-                            {
-                                case "-cs":
-                                case "-ts":
-                                    {
-                                        ++i;
-                                        var name = NextArg(ref i, args, optional: true);
-                                        if (name != null)
-                                        {
-                                            var lastDot = arg == "-cs" ? name.LastIndexOf('.') : -1;
-                                            if (lastDot == -1)
-                                            {
-                                                className = name;
-                                            }
-                                            else
-                                            {
-                                                space = name.Substring(0, lastDot);
-                                                className = name.Substring(lastDot + 1);
-                                            }
-                                        }
-                                        outType = arg.Substring(1);
-                                    }
-                                    break;
-                                case "-o":
-                                    {
-                                        ++i;
-                                        outPath = NextArg(ref i, args);
-                                    }
-                                    break;
-                                default:
-                                    Usage();
-                                    break;
-                            }
-                        }
+                        Usage();
                     }
                 }
             }
