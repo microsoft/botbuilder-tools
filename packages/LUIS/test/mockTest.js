14382b5c39b61cee716183ba0bf553a7321a2737
JuanAr <juan.arguello@southworks.com>
2018-10-26 16:18:13 -0300

    Implement mock test recorder

--- packages/LUIS/test/mockTest.js
+++ packages/LUIS/test/mockTest.js
@@ -1,18 +1,45 @@
 const fs = require('fs-extra');
-const nock = require('nock');
 const path = require('path');
+const nock = require('nock');
 const pathToMockFile = path.resolve(process.argv[2]);
-const pathToCLITool = path.resolve(process.argv[3]);
-const mockData = fs.readJSONSync(pathToMockFile);
+const pathToCLITool = process.argv[3];
+const shouldMock = true;
+process.argv.splice(1,2);
 
-for (const key in mockData) {
-	if (mockData.hasOwnProperty(key)) {
-		const element = mockData[key];
-		nock(element.url)
-		.intercept(new RegExp(element.uri, "i"), element.method)
-		.reply(element.responseCode,element.response);
+if(!fs.existsSync(pathToMockFile) && shouldMock){
+	var folderPath = pathToMockFile.substring(0,pathToMockFile.lastIndexOf(path.sep)+1);
+	if (!fs.existsSync(folderPath)){
+		fs.mkdirSync(folderPath);
 	}
+	fs.writeFileSync(pathToMockFile,"[]");
+	nock.recorder.rec({
+		output_objects: true,
+		dont_print: false,
+		enable_reqheaders_recording:true,
+		logging:(msg)=>{
+			saveMockDataToFile(msg);
+		},
+		use_separator:false
+	});
+	require(pathToCLITool);
+}else{
+	if(shouldMock){
+		const mockData = fs.readJSONSync(pathToMockFile);
+		for (const key in mockData) {
+			if (mockData.hasOwnProperty(key)) {
+				const element = mockData[key];
+				nock(element.scope)
+				.intercept(element.path, element.method)
+				.reply(element.status,element.response);
+			}
+		}
+	}
+	require(pathToCLITool);
 }
 
-process.argv.splice(1,2); //The args would look like it was run directly from the CLI
-require(pathToCLITool);
+function saveMockDataToFile(data){
+	var reqs = fs.readJSONSync(pathToMockFile);
+	reqs.push(data);
+	fs.writeJSON(pathToMockFile,reqs,{spaces:'\t'})
+	nock.recorder.play();
+}
