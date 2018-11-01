d9fe11267ff74e401a7bf74a9e11a9172d1df6ce
Vishwac Sena Kannan <vkannan@microsoft.com>
2018-10-31 20:24:46 -0700

    Fix for PR comment.

--- packages/Ludown/lib/ludown-parse-ToLuis.js
+++ packages/Ludown/lib/ludown-parse-ToLuis.js
@@ -34,7 +34,8 @@ if (process.argv.length < 4) {
     program.help();
 } else {
     if (program.luis_culture) {
-        // List of supported LUIS.ai locales
+        // List of supported LUIS.ai locales. 
+        // From: https://docs.microsoft.com/en-us/azure/cognitive-services/luis/luis-language-support
         const LUISLocales = ['en-us', 'fr-ca', 'zh-cn', 'nl-nl', 'fr-fr', 'de-de', 'it-it', 'ja-jp', 'ko-kr', 'pt-br', 'es-es', 'es-mx'];
         if (!(LUISLocales.includes(program.luis_culture.toLowerCase()))) {
             process.stderr.write(chalk.default.yellowBright(`\nWARN: Unrecognized LUIS locale. Supported locales are - ${LUISLocales.toString()} \n\n`));
