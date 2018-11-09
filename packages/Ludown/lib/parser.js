f1643788adea60818ff7eaa801c4d83237e87ca3
Vishwac Sena Kannan <vishwacsenakannan@Vishwacs-MBP.guest.corp.microsoft.com>
2018-11-09 13:17:31 -0800

    Fix for #744 and #755; updated existing tests and added new tests.

--- packages/Ludown/lib/parser.js
+++ packages/Ludown/lib/parser.js
@@ -370,9 +370,15 @@ const resolveReferencesInUtterances = async function(allParsedContent) {
                     // find the parsed file
                     let parsedLUISBlob = (allParsedContent.LUISContent || []).find(item => item.srcFile == parsedUtterance.luFile);
                     if(parsedLUISBlob === undefined) throw (new exception(retCode.errorCode.INVALID_INPUT,`[ERROR] Unable to parse ${utterance.text} in file: ${luisModel.srcFile}`));
-                    // get utterance list from reference intent and update list
-                    let referenceIntent = parsedUtterance.ref.replace(/-/g, ' ').trim();
-                    let utterances = parsedLUISBlob.LUISJsonStructure.utterances.filter(item => item.intent == referenceIntent);
+                    let utterances;
+                    if (parsedUtterance.ref.endsWith('utterances')) {
+                        // get all utterances and add them
+                        utterances = parsedLUISBlob.LUISJsonStructure.utterances;
+                    } else {
+                        // get utterance list from reference intent and update list
+                        let referenceIntent = parsedUtterance.ref.replace(/-/g, ' ').trim();
+                        utterances = parsedLUISBlob.LUISJsonStructure.utterances.filter(item => item.intent == referenceIntent);
+                    }
                     (utterances || []).forEach(item => newUtterancesToAdd.push(new hClasses.uttereances(item.text, utterance.intent)));
                     // find and add any patterns for this intent
                     let patterns = parsedLUISBlob.LUISJsonStructure.patterns.filter(item => item.intent == referenceIntent);
