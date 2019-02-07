1b95b74cb5148051388940012b0aefaffe164323
omsour <omsour@microsoft.com>
2019-02-06 22:04:27 -0800

    Added invalid command validator

---
+++ packages/Ludown/src/utils/validators/invalid-command-validator.ts
@@ -0,0 +1,22 @@
+import { IValidatorFactory } from '../../interfaces/utils/validators/IValidatorFactory';
+import { ERROR_CODE } from '../../models/error-codes';
+
+/**
+ * @description
+ * Generates a factory function that validates whether the given command matches one
+ * of the factory created list of allowable commands.
+ *
+ * @param allowableCommands The list of allowable commands to check the program command
+ * state against.
+ * @returns Promise of true on resolve and an IValidatorErrorObject on rejection.
+ */
+export const invalidCommandValidatorFactory: IValidatorFactory = (allowableCommands: string[]) => {
+    return {
+        execute: (command: string) => {
+            return new Promise((resolve, reject) => allowableCommands.includes(command) ?
+                resolve(true) :
+                reject({ code: ERROR_CODE.UNKNOWN_COMMAND, data: command })
+            );
+        }
+    };
+};
---
+++ packages/Ludown/tests/validators/invalid-command-validator.spec.ts
@@ -0,0 +1,20 @@
+import { ERROR_CODE } from '../../src/models/error-codes';
+import { invalidCommandValidatorFactory } from '../../src/utils/validators/invalid-command-validator';
+
+describe('Invalid command validator', () => {
+    it('should resolve when given command is allowed', (done) => {
+        invalidCommandValidatorFactory(['command1', 'command2', 'command3']).execute('command2')
+            .then(value => {
+                expect(value).toBeTruthy();
+                done();
+            });
+    });
+
+    it('should reject when given command is not in the allowed list', (done) => {
+        invalidCommandValidatorFactory(['command1', 'command2', 'command3']).execute('command4')
+            .catch(err => {
+                expect(err).toEqual({ code: ERROR_CODE.UNKNOWN_COMMAND, data: 'command4' });
+                done();
+            });
+    });
+});
