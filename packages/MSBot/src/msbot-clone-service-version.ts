2d55d307b9434649c7b3b173d231d15326264934
nurih <1554735+nurih@users.noreply.github.com>
2019-02-15 12:40:57 -0800

    Version parser allows 1 - 4 elements major[.minor[.patch[.revision]]]

--- packages/MSBot/src/msbot-clone-service-version.ts
+++ packages/MSBot/src/msbot-clone-service-version.ts
@@ -1,20 +1,22 @@
 export class ServiceVersion {
     constructor(version: string) {
-        const versionPattern = /([0-9]+)\.([0-9]+)\.([0-9]+)/;
-        const versions = versionPattern.exec(version) || ['0', '0', '0', '0'];
+        const versionPattern = /([0-9]+)(?:\.([0-9]+)(?:\.([0-9]+)(?:\.([0-9]+))?)?)?/;
+        const versions = versionPattern.exec(version) || ['0', '0', '0', '0', '0'];
         this.major = parseInt(versions[1]);
-        this.minor = parseInt(versions[2]);
-        this.patch = parseInt(versions[3]);
+        this.minor = parseInt(versions[2]) || 0;
+        this.patch = parseInt(versions[3]) || 0;
+        this.revision = parseInt(versions[4]) || 0;
     }
 
     public major: number;
     public minor: number;
     public patch: number;
+    public revision: number;
 
     public isOlder(version: ServiceVersion): boolean {
 
-        let thisVersion: number[] = [this.major, this.minor, this.patch];
-        let theirVersion: number[] = [version.major, version.minor, version.patch];
+        let thisVersion: number[] = [this.major, this.minor, this.patch, this.revision];
+        let theirVersion: number[] = [version.major, version.minor, version.patch, version.revision];
 
         for (let i = 0; i < thisVersion.length; i++) {
             if (thisVersion[i] < theirVersion[i]) {
--- packages/MSBot/test/msbot.clone.test.suite.js
+++ packages/MSBot/test/msbot.clone.test.suite.js
@@ -5,48 +5,93 @@ describe("ServiceVersion", () => {
 
     it("parses version strings", async () => {
 
-        versions = [
-            "1.2.3",
-            "(1.2.3)",
-            "   1.2.3\r",
-            "\t1.2.3\t"
+        versions = [{
+                text: "1",
+                major: 1,
+                minor: 0,
+                patch: 0,
+                rev: 0
+            }, {
+                text: "1.2",
+                major: 1,
+                minor: 2,
+                patch: 0,
+                rev: 0
+            }, {
+                text: "1.2.3",
+                major: 1,
+                minor: 2,
+                patch: 3,
+                rev: 0
+            },
+            {
+                text: "(1.2.3)",
+                major: 1,
+                minor: 2,
+                patch: 3,
+                rev: 0
+            },
+            {
+                text: "   1.2.3\r",
+                major: 1,
+                minor: 2,
+                patch: 3,
+                rev: 0
+            },
+            {
+                text: "\t1.2.3\t",
+                major: 1,
+                minor: 2,
+                patch: 3,
+                rev: 0
+            },
+            {
+                text: "1.2.3.4",
+                major: 1,
+                minor: 2,
+                patch: 3,
+                rev: 4
+            }
         ];
 
-        versions.forEach(version => {
+        versions.forEach(item => {
 
-            const actual = new ServiceVersion(version);
+            const actual = new ServiceVersion(item.text);
 
-            assert.deepEqual(actual.major, 1, `Unexpected major version ${actual.major} parsed from ${version} ${JSON.stringify(actual)}`);
-            assert.deepEqual(actual.minor, 2, `Unexpected minor version ${actual.minor} parsed from ${version} ${JSON.stringify(actual)}`);
-            assert.deepEqual(actual.patch, 3, `Unexpected patch version ${actual.patch} parsed from ${version} ${JSON.stringify(actual)}`);
+            assert.deepEqual(actual.major, item.major, `Unexpected major ${actual.major} parsed from ${item} ${JSON.stringify(actual)}`);
+            assert.deepEqual(actual.minor, item.minor, `Unexpected minor ${actual.minor} parsed from ${item} ${JSON.stringify(actual)}`);
+            assert.deepEqual(actual.patch, item.patch, `Unexpected patch ${actual.patch} parsed from ${item} ${JSON.stringify(actual)}`);
+            assert.deepEqual(actual.revision, item.rev, `Unexpected revision  ${actual.revision} parsed from ${item} ${JSON.stringify(actual)}`);
         });
     });
 
     it('is older when version is strictly older', () => {
-        const newer = new ServiceVersion("11.22.33");
+        const newer = new ServiceVersion("11.22.33.44");
 
-        const older = ["11.22.00", "11.21.99", "10.999.999"];
+        const older = ["10", "11.00", "11.22.00", "11.21.99", "10.999.999", "11.22.33.00", "10.99.99.99"];
 
         older.forEach(old => {
-            const older = new ServiceVersion(old);
-            assert.ok(older.isOlder(newer), `${JSON.stringify(older)} *not* older than ${JSON.stringify(newer)}`);
+            const cases = new ServiceVersion(old);
+            assert.ok(cases.isOlder(newer), `${JSON.stringify(cases)} *not* older than ${JSON.stringify(newer)}`);
         })
     })
 
     it('is not older when version is strictly newer', () => {
         const older = new ServiceVersion("11.22.33");
 
-        const newer = ["11.22.99", "11.23.0", "12.0.0"];
+        const cases = ["12", "11.23", "11.22.99", "11.23.0", "12.0.0", "11.22.33.99", "11.23.0.0", "12.0.0.0"];
 
-        newer.forEach(newVersion => {
+        cases.forEach(newVersion => {
             const actual = new ServiceVersion(newVersion);
             assert.ok(!actual.isOlder(older), `${JSON.stringify(actual)} *is* older than ${JSON.stringify(older)}`);
         })
     })
 
     it('is not older than self', () => {
-        const actual = new ServiceVersion("11.22.33");
-
-        assert.ok(!actual.isOlder(actual));
+        const cases = ["11", "11.22", "11.22.33", "11.22.33.44"]
+        cases.forEach(version => {
+            const actual = new ServiceVersion(version);
+            assert.ok(!actual.isOlder(actual));
+        });
     })
 });
