b3c583eaf61c546b5b194a4a62d2ccff6c732d27
omsour <omsour@microsoft.com>
2019-02-22 15:54:00 -0800

    Added intent data interface as a DTO interface between intent renderer and intents section renderer

--- packages/Ludown/src/interfaces/luis/intents/IIntent.ts
+++ packages/Ludown/src/interfaces/luis/intents/IIntent.ts
@@ -1,7 +1,20 @@
+import { IPattern } from '../patterns/IPattern';
+import { IUtterance } from '../utterances/IUtterance';
+
 /**
  * @description
  * Represents a LUIS intent.
  */
 export interface IIntent {
     name: string;
+}
+
+/**
+ * @description
+ * Represnts data that is relevant to an intent.
+ */
+export interface IIntentData {
+    utterances: IUtterance[];
+
+    patterns: IPattern[];
 }
