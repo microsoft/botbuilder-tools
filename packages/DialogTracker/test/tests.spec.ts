d9791c769cb9d408aaf42ebcfa58af054e4ded0f
Chris McConnell <chrimc@microsoft.com>
2019-03-04 17:47:02 -0800

    Fix test errors from rename.

--- packages/DialogTracker/test/tests.spec.ts
+++ packages/DialogTracker/test/tests.spec.ts
@@ -5,22 +5,22 @@
  */
 // tslint:disable:no-console
 // tslint:disable:no-object-literal-type-assertion
-import * as ct from '../src/dialogTracker';
+import * as dt from '../src/DialogTracker';
 import { expect } from 'chai';
 import * as fs from 'fs-extra';
 import * as glob from 'globby';
 import 'mocha';
 import * as path from 'path';
-let cs = require('cogschema/lib/cogSchema');
+let cs = require('Dialogschema/lib/DialogSchema');
 
 describe('Test .dialog indexing library', async () => {
-    let schemas = new ct.SchemaTracker();
-    let tracker = new ct.dialogTracker(schemas);
+    let schemas = new dt.SchemaTracker();
+    let tracker = new dt.DialogTracker(schemas);
 
     before(async () => {
         // If you want to regenerate the oracle *.schema and *.lg files
         // 1) Run schemas/makeschemas.cmd
-        // 2) Run cogLint examples/*.dialog -w examples/app.lg (or execute "Launch program")
+        // 2) Run DialogLint examples/*.dialog -w examples/app.lg (or execute "Launch program")
         await fs.remove("test.out");
         await fs.mkdirp("test.out");
         for(let file of glob.sync(["test/**/*.schema", "test/**/*.lg", "test/**/*.dialog", "test/**/*.cmd"])) {
@@ -33,7 +33,7 @@ describe('Test .dialog indexing library', async () => {
         await cs.mergeSchemas(["schemas/*.schema"], "examples/app.schema", true);
         await cs.mergeSchemas(["schemas/prompt.schema"], "examples\promptOnly.schema", true);
         tracker.root = process.cwd();
-        await tracker.addCogFiles(["examples/*.dialog"]);
+        await tracker.addDialogFiles(["examples/*.dialog"]);
         await tracker.writeLG("examples/app.lg");
     });
 
@@ -50,20 +50,20 @@ describe('Test .dialog indexing library', async () => {
     it(`types`, () => expect(tracker.schema.typeToType.size).equal(5));
 
     it('clone', async () => {
-        let foo = tracker.cloneCog("foo");
+        let foo = tracker.cloneDialog("foo");
         expect(foo, "Can't find dialog").to.equal(undefined);
-        let original = tracker.findCog("root");
+        let original = tracker.findDialog("root");
         expect(original, "Can't find dialog").to.not.equal(undefined);
-        let copy = tracker.cloneCog("root");
+        let copy = tracker.cloneDialog("root");
         if (original && copy) {
             let len = original.body.recognizers.length;
             copy.body.recognizers.pop();
             expect(len === copy.body.recognizers.length + 1).is.true;
-            await tracker.updateCog(copy);
-            let newCog = tracker.findCog("root");
-            if (newCog) {
-                expect(copy, "Update should be object").is.equal(newCog);
-                expect(newCog.save, "Saved should be true").is.true;
+            await tracker.updateDialog(copy);
+            let newDialog = tracker.findDialog("root");
+            if (newDialog) {
+                expect(copy, "Update should be object").is.equal(newDialog);
+                expect(newDialog.save, "Saved should be true").is.true;
                 verify(tracker);
             } else {
                 expect.fail("Did not update");
@@ -75,12 +75,12 @@ describe('Test .dialog indexing library', async () => {
 
     it('write', async () => {
         let savesBefore = size(tracker.dialogs.filter((c) => c.save));
-        await tracker.writeCogs("cogs");
+        await tracker.writeDialogs("Dialogs");
         let savesAfter = size(tracker.dialogs.filter((c) => c.save));
         expect(savesAfter).equals(0);
         let saved = 0;
-        for(let file of glob.sync("cogs/examples/*.dialog")) {
-            let dialog = tracker.findCogFile(file);
+        for(let file of glob.sync("Dialogs/examples/*.dialog")) {
+            let dialog = tracker.findDialogFile(file);
             expect(dialog, `${dialog} is not found as ${file}`).is.not.equal(undefined);
             ++saved;
         }
@@ -89,7 +89,7 @@ describe('Test .dialog indexing library', async () => {
 
     it('remove', () => {
         for (let dialog of tracker.dialogs) {
-            tracker.removeCog(dialog);
+            tracker.removeDialog(dialog);
             verifyRemoved(tracker, dialog);
         }
     });
@@ -114,13 +114,13 @@ function size<T>(iterable: Iterable<T>): number {
     return i;
 }
 
-function verify(tracker: ct.dialogTracker) {
+function verify(tracker: dt.DialogTracker) {
     for (let def of tracker.allDefinitions()) {
         checkDef(def, tracker);
     }
 }
 
-function checkDef(def: ct.Definition, tracker: ct.dialogTracker): void {
+function checkDef(def: dt.Definition, tracker: dt.DialogTracker): void {
     if (def.id) {
         expect(findDefinition(tracker.idToDef.get(def.id), def), `${def} in idTo`).is.true;
     }
@@ -134,7 +134,7 @@ function checkDef(def: ct.Definition, tracker: ct.dialogTracker): void {
     }
 }
 
-function findDefinition(definitions: undefined | ct.Definition[], definition: ct.Definition): boolean {
+function findDefinition(definitions: undefined | dt.Definition[], definition: dt.Definition): boolean {
     let ok = false;
     if (definitions) {
         ok = definitions.findIndex((d) => d.compare(definition) == 0) != -1;
@@ -142,7 +142,7 @@ function findDefinition(definitions: undefined | ct.Definition[], definition: ct
     return ok;
 }
 
-function verifyRemoved(tracker: ct.dialogTracker, dialog: ct.dialog) {
+function verifyRemoved(tracker: dt.DialogTracker, dialog: dt.Dialog) {
     for (let def of tracker.allDefinitions()) {
         expect(def.dialog).not.equal(dialog);
         for (let used of def.usedBy) {
