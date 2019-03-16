f7ef0ea22bc33641f50cbef317943ad848db9a6c
omsour <omsour@microsoft.com>
2019-03-16 15:42:14 -0700

    Added interfaces for typed files and file types

---
+++ packages/Ludown/src/interfaces/helpers/ITypedFile.ts
@@ -0,0 +1,11 @@
+import { FILE_TYPE } from '../../models/file-types';
+import { IInputFile } from './IInputFile';
+
+/**
+ * @description
+ * Represents a file read of input with the its type information
+ * known and appended.
+ */
+export interface ITypedFile extends IInputFile {
+	fileType: FILE_TYPE;
+}
---
+++ packages/Ludown/src/models/file-types.ts
@@ -0,0 +1,10 @@
+/**
+ * @description
+ * An enumeration of the supported file types that can be imported
+ * or exported via the tool.
+ */
+export enum FILE_TYPE {
+	LUIS,
+	QNA,
+	QNA_ALTERATION
+}
