e934a5ca10cefa2d52ef6086208622ad4ffe3ccd
Hongyang Du <hond@microsoft.com>
2019-05-27 10:52:22 +0800

    add templateName code completion feature

--- packages/LGvscodeExt/src/providers/lgCompletionItemProvider.ts
+++ packages/LGvscodeExt/src/providers/lgCompletionItemProvider.ts
@@ -8,6 +8,7 @@
 
 import * as vscode from 'vscode';
 import * as constants from '../constants';
+import { TemplateEngine } from 'botbuilder-lg';
 
 /**
  * Code completions provide context sensitive suggestions to the user.
@@ -21,6 +22,11 @@ export class LGCompletionItemProvider implements vscode.CompletionItemProvider {
         position: vscode.Position,
         token: vscode.CancellationToken,
         context: vscode.CompletionContext): vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList> {
-        return constants.buildInfunctionNames.map(item => new vscode.CompletionItem(item));
+        const buildInfunctionItem: vscode.CompletionItem[] = constants.buildInfunctionNames.map(item => new vscode.CompletionItem(item));
+        let engine: TemplateEngine = TemplateEngine.fromText(document.getText());
+
+        const templateNameItem: vscode.CompletionItem[] = engine.templates.map(item => new vscode.CompletionItem(item.Name));
+
+        return [...buildInfunctionItem, ...templateNameItem];
     }
 }
