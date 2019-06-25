7b11fd72dca08cfb28a133f02a87d4233bbf5434
Hongyang Du <hond@microsoft.com>
2019-06-25 16:04:23 +0800

    modify functions

--- packages/LGvscodeExt/src/buildinFunctions.ts
+++ packages/LGvscodeExt/src/buildinFunctions.ts
@@ -11,12 +11,12 @@ export const buildInfunctionNames: string[] = [
     'min','max','average','sum','exists','length','replace','replaceIgnoreCase','split','substring','toLower','toUpper',
     'trim','count','contains','contains','join','first','last','foreach','addDays','addHours','addMinutes','addSeconds',
     'dayOfMonth','dayOfWeek','dayOfYear','month','date','year','utcNow','formatDateTime','subtractFromTime','dateReadBack',
-    'getTimeOfDay','float','int','string','bool','Accessor','Element','createArray','Constant','Lambda','if','rand','json',
+    'getTimeOfDay','float','int','string','bool','createArray','Constant','if','rand','json',
     'getProperty','addProperty','removeProperty','setProperty','endsWith','startsWith','countWord','addOrdinal','guid',
     'indexOf','lastIndexOf','union','intersection ','skip','take','filterNotEqual','subArray','array','binary','dataUri',
     'dataUriToBinary','dataUriToString','decodeUriComponent','base64','base64ToBinary','base64ToString','uriComponent',
-    'uriComponentToString','xml','range','getFutureTime','getPastTime','addToTime','convertFromUtc','convertTimeZone',
-    'convertToUtc','startOfDay','startOfHour','startOfMonth','ticks','uriHost','uriPath','uriPathAndQuery','uriPort',
+    'uriComponentToString','xml','range','getFutureTime','getPastTime','addToTime','convertFromUtc', 'convertToUtc','startOfDay',
+    'startOfHour','startOfMonth','ticks','uriHost','uriPath','uriPathAndQuery','uriPort',
     'uriQuery','uriScheme','coalesce','xpath',
 ];
 
