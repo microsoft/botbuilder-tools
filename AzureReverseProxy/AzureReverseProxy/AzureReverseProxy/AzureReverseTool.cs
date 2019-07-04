749260beff7650a3a9396b7073b3302ea90101b8
VictorGrycuk <victor@grycuk.net>
2019-07-04 14:14:53 -0300

    Add deployment methods

--- AzureReverseProxy/AzureReverseProxy/AzureReverseProxy/AzureReverseTool.cs
+++ AzureReverseProxy/AzureReverseProxy/AzureReverseProxy/AzureReverseTool.cs
@@ -1,8 +1,131 @@
-using System;
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Azure.Management.Relay;
+using Microsoft.Azure.Management.ResourceManager;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.ResourceManager.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Reflection;
+using ResourceManagementClient = Microsoft.Azure.Management.ResourceManager.ResourceManagementClient;
 
 namespace AzureReverseProxy
 {
     public class AzureReverseTool
     {
+        private readonly IConfiguration config;
+        private readonly AzureCredentials credentials;
+
+        public AzureReverseTool(IConfiguration config)
+        {
+            this.config = config;
+
+            // Try to obtain the service credentials
+            credentials = new AzureCredentialsFactory().FromServicePrincipal(
+                            config.ClientId,
+                            config.ClientSecret,
+                            config.TenantId,
+                            AzureEnvironment.AzureGlobalCloud);
+        }
+
+        public void GenerateRelayResource()
+        {
+            // Read the template and parameter file contents
+            JObject templateFileContents = GetJsonFileContents("AzureReverseProxy.template.json");
+            JObject parameterFileContents = GetJsonFileContents("AzureReverseProxy.parameters.json");
+
+            // Assign the deplyment name specified by the user
+            parameterFileContents["parameters"]["namespaces_Relay_Demo_name"]["value"] = config.DeploymentName;
+
+            // Create the resource manager client
+            ResourceManagementClient resourceManagementClient = new ResourceManagementClient(credentials)
+            {
+                SubscriptionId = config.SubscriptionId
+            };
+
+            // Create or check that resource group exists
+            EnsureResourceGroupExists(resourceManagementClient, config.ResourceGroupName, config.ResourceGroupLocation);
+
+            // Start a deployment
+            DeployTemplate(resourceManagementClient, config.ResourceGroupName, config.DeploymentName, templateFileContents, parameterFileContents);
+        }
+
+        /// <summary>
+        /// Reads a JSON file from the specified path
+        /// </summary>
+        /// <param name="pathToJson">The full path to the JSON file</param>
+        /// <returns>The JSON file contents</returns>
+        private static JObject GetJsonFileContents(string fileName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using (Stream stream = assembly.GetManifestResourceStream(fileName))
+            using (StreamReader file = new StreamReader(stream))
+            {
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+                    return (JObject)JToken.ReadFrom(reader);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Ensures that a resource group with the specified name exists. If it does not, will attempt to create one.
+        /// </summary>
+        /// <param name="resourceManagementClient">The resource manager client.</param>
+        /// <param name="resourceGroupName">The name of the resource group.</param>
+        /// <param name="resourceGroupLocation">The resource group location. Required when creating a new resource group.</param>
+        private async static void EnsureResourceGroupExists(ResourceManagementClient resourceManagementClient, string resourceGroupName, string resourceGroupLocation)
+        {
+            if (await resourceManagementClient.ResourceGroups.CheckExistenceAsync(resourceGroupName) != true)
+            {
+                // Console.WriteLine(string.Format("Creating resource group '{0}' in location '{1}'", resourceGroupName, resourceGroupLocation));
+                var resourceGroup = new ResourceGroup
+                {
+                    Location = resourceGroupLocation
+                };
+
+                await resourceManagementClient.ResourceGroups.CreateOrUpdateAsync(resourceGroupName, resourceGroup);
+            }
+            else
+            {
+                // Console.WriteLine(string.Format("Using existing resource group '{0}'", resourceGroupName));
+            }
+        }
+
+        /// <summary>
+        /// Starts a template deployment.
+        /// </summary>
+        /// <param name="resourceManagementClient">The resource manager client.</param>
+        /// <param name="resourceGroupName">The name of the resource group.</param>
+        /// <param name="deploymentName">The name of the deployment.</param>
+        /// <param name="templateFileContents">The template file contents.</param>
+        /// <param name="parameterFileContents">The parameter file contents.</param>
+        private static void DeployTemplate(ResourceManagementClient resourceManagementClient, string resourceGroupName, string deploymentName, JObject templateFileContents, JObject parameterFileContents)
+        {
+            var deployment = new Deployment
+            {
+                Properties = new DeploymentProperties
+                {
+                    Mode = DeploymentMode.Incremental,
+                    Template = templateFileContents,
+                    Parameters = parameterFileContents["parameters"].ToObject<JObject>()
+                }
+            };
+
+            var deploymentResult = resourceManagementClient.Deployments.CreateOrUpdate(resourceGroupName, deploymentName, deployment);
+            // Console.WriteLine(string.Format("Deployment status: {0}", deploymentResult.Properties.ProvisioningState));
+        }
+
+        public AuthorizationRule GetAuthorizationRule(string ruleName)
+        {
+            RelayManagementClient relayMC = new RelayManagementClient(credentials) { SubscriptionId = config.SubscriptionId };
+
+            var response = relayMC.Namespaces.ListKeysWithHttpMessagesAsync(config.ResourceGroupName, config.DeploymentName, ruleName).Result.Response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            return JsonConvert.DeserializeObject<AuthorizationRule>(response);
+        }
     }
 }
--- AzureReverseProxy/AzureReverseProxy/AzureReverseProxyCMD/Program.cs
+++ AzureReverseProxy/AzureReverseProxy/AzureReverseProxyCMD/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using System;
+using System.IO;
+using AzureReverseProxy;
+using Newtonsoft.Json;
 
 namespace AzureReverseProxyCMD
 {
@@ -6,7 +9,19 @@ namespace AzureReverseProxyCMD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var az = new AzureReverseTool(GetConfiguration());
+            az.GenerateRelayResource();
+        }
+
+        private static IConfiguration GetConfiguration()
+        {
+            var folder = AppDomain.CurrentDomain.BaseDirectory;
+            var file = "appsettings.json";
+
+            using (StreamReader reader = new StreamReader(Path.Combine(folder, file)))
+            {
+                return JsonConvert.DeserializeObject<Configuration>(reader.ReadToEnd());
+            }
         }
     }
 }
