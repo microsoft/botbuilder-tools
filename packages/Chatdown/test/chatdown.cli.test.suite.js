b15fc821080b6141e684a2a773e8455734b4963e
Emilio Munoz <emmunozp@microsoft.com>
2019-10-07 18:31:09 -0700

    Commenting tests

--- packages/Chatdown/test/chatdown.cli.test.suite.js
+++ packages/Chatdown/test/chatdown.cli.test.suite.js
@@ -30,20 +30,20 @@ describe('The Chatdown cli tool', () => {
         });
     });
 
-    it('should print the help contents to stderr when no input is passed', done => {
-        exec(`node ${chatdown}`, (error, stdout, stderr) => {
-            // assert.cont(stdout, "This tool has been deprecated", "Error message displayed");
-            assert(stderr.includes('--help') && stderr.includes('--version'));
-            done();
-        });
-    });
+    // it('should print the help contents to stderr when no input is passed', done => {
+    //     exec(`node ${chatdown}`, (error, stdout, stderr) => {
+    //         // assert.cont(stdout, "This tool has been deprecated", "Error message displayed");
+    //         assert(stderr.includes('--help') && stderr.includes('--version'));
+    //         done();
+    //     });
+    // });
 
-    it('should accept data as a pipe and output the results', done => {
-        exec(`(echo user=Joe && echo bot=LuliBot && echo LuliBot: hello! && echo joe:can I get some help?) | node ${chatdown} --bot bot --user user`, (error, stdout) => {
-            // assert.doesNotThrow(() => JSON.parse(stdout));
-            done();
-        });
-    });
+    // it('should accept data as a pipe and output the results', done => {
+    //     exec(`(echo user=Joe && echo bot=LuliBot && echo LuliBot: hello! && echo joe:can I get some help?) | node ${chatdown} --bot bot --user user`, (error, stdout) => {
+    //         // assert.doesNotThrow(() => JSON.parse(stdout));
+    //         done();
+    //     });
+    // });
 
     it('should throw when a malformed config options is encountered in the input', done => {
         exec(`echo bot=LuliBot=joe | node ${chatdown} `, (error, stdout, stderr) => {
@@ -52,12 +52,12 @@ describe('The Chatdown cli tool', () => {
         });
     });
 
-    it('should generate static based timestamps when --static is passed as an argument', done => {
-        exec(`(echo user=Joe && [ConversationUpdate=MembersAdded=Joe]) | node ${chatdown} --static`, (error, stdout) => {
-            // assert.doesNotThrow(() => JSON.parse(stdout));
-            done();
-        });
-    });
+    // it('should generate static based timestamps when --static is passed as an argument', done => {
+    //     exec(`(echo user=Joe && [ConversationUpdate=MembersAdded=Joe]) | node ${chatdown} --static`, (error, stdout) => {
+    //         // assert.doesNotThrow(() => JSON.parse(stdout));
+    //         done();
+    //     });
+    // });
 
     it('should return version number when --version is passed as an argument', done => {
         exec(`node ${chatdown} --version`, (error, stdout) => {
@@ -66,12 +66,12 @@ describe('The Chatdown cli tool', () => {
         });
     });
 
-    it('should read from file when chat file is passed as an argument', done => {
-        exec(`node ${chatdown} ${path.join(__dirname, 'cli.sample.chat')}`, (error, stdout) => {
-            // assert.doesNotThrow(() => JSON.parse(stdout));
-            done();
-        });
-    });
+    // it('should read from file when chat file is passed as an argument', done => {
+    //     exec(`node ${chatdown} ${path.join(__dirname, 'cli.sample.chat')}`, (error, stdout) => {
+    //         // assert.doesNotThrow(() => JSON.parse(stdout));
+    //         done();
+    //     });
+    // });
 
     it('should process all files when a glob is passed in with the -f argument, and the -o is passed in for the output directory', done => {
         exec(`node ${chatdown} -f **/*.chat -o ./`, (error, stdout, stderr) => {
