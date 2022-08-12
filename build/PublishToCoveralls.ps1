edfb3e83b7f6205775e2ceb31bc7817c89fcf251
Bruce Haley <v-brucehaley@microsoft.com>
2022-08-12 16:54:00 -0700

    Update PublishToCoveralls.ps1

--- build/PublishToCoveralls.ps1
+++ build/PublishToCoveralls.ps1
@@ -3,15 +3,20 @@
 # Arguments example: -coverallsToken $(Coveralls.Token) -pathToCoverageFiles $(Build.SourcesDirectory)\CodeCoverage
 #
 Param(
-    [string]$coverallsToken,
+    [string]$coverallsToken = '',  # This arg will display in the Azure pipeline log. To hide it, pass it as environment var CoverallsToken instead.
     [string]$pathToCoverageFiles,
     [string]$serviceName = 'Azure DevOps'
 )
 
 Write-Host Install tools
 $basePath = (get-item $pathToCoverageFiles ).parent.FullName
-$coverageAnalyzer = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe"
-dotnet tool install coveralls.net --version 1.0.0 --tool-path tools
+
+$coverageAnalyzer = "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe"
+if (-not (Test-Path $coverageAnalyzer)) {
+  $coverageAnalyzer = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe"
+}
+
+dotnet tool install coveralls.net --version 4.0.1 --tool-path tools --add-source https://api.nuget.org/v3/index.json
 $coverageUploader = ".\tools\csmacnz.Coveralls.exe"
 
 Write-Host "Analyze coverage [$coverageAnalyzer] with args:"
@@ -24,9 +29,15 @@ $analyzeArgs += $coverageFiles
 Foreach ($i in $analyzeArgs) { Write-Host "  $i" }
 ."$coverageAnalyzer" @analyzeArgs
 
+# Get $coverallsToken from environment var
+if ($coverallsToken -eq '') {
+    $coverallsToken = $Env:CoverallsToken;
+}
+
 Write-Host "Upload coverage [$coverageUploader] with args"
 if (Test-Path env:System_PullRequest_SourceBranch) {
-    $branchName = $env:System_PullRequest_SourceBranch -replace "refs/heads/", "" }
+    $branchName = $env:System_PullRequest_SourceBranch -replace "refs/heads/", "" 
+}
 $uploadArgs = @(
     "--dynamiccodecoverage",
     "-i ""$pathToCoverageFiles\coverage.coveragexml""",
@@ -37,21 +48,24 @@ $uploadArgs = @(
     "--jobId ""$env:Build_BuildId""",
     "--commitId ""$env:Build_SourceVersion""",
     "--commitAuthor ""$env:Build_RequestedFor""",
-    "--commitEmail ""$env:Build_RequestedForEmail""",
+    "--commitEmail ""$env:Build_RequestedForEmail """,
     "--commitMessage ""$env:Build_SourceVersionMessage""",
     "--serviceName ""$serviceName"""
 );
-if (Test-Path env:System_PullRequest_SourceBranch)
-{
+if (Test-Path env:System_PullRequest_SourceBranch) {
     $uploadArgs += "--commitBranch ""$($env:System_PullRequest_SourceBranch -replace ""refs/heads/"", """")"""
 }
-else
-{
+else {
     $uploadArgs += "--commitBranch ""$($env:Build_SourceBranch -replace ""refs/heads/"", """")"""
 }
-if (Test-Path env:System_PullRequest_PullRequestNumber)
-{
+if (Test-Path env:System_PullRequest_PullRequestNumber) {
     $uploadArgs += "--pullRequest ""$env:System_PullRequest_PullRequestNumber"""
 }
-Foreach ($i in $uploadArgs) { Write-Host "  $i" }
+Foreach ($i in $uploadArgs) { 
+    if (-not $i.StartsWith("--repoToken")) {
+        Write-Host "  $i";
+    } else {
+        Write-Host "  --repoToken ******";
+    }
+}
 Start-Process $coverageUploader -ArgumentList $uploadArgs -NoNewWindow
